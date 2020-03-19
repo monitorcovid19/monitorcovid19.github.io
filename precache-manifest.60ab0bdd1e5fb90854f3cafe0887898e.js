e72dcc00f93d86d1790d6fe6cffa6a1ea795b40e
Otacilio Saraiva Maia Neto <otacilio.n97@gmail.com>
2020-03-19 14:06:06 -0300

    Add files via upload

---
+++ precache-manifest.60ab0bdd1e5fb90854f3cafe0887898e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d983ea1e5552168eabb0eaf597036de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9fda492d331374c0be5",
+    "url": "/static/css/main.4665a683.chunk.css"
+  },
+  {
+    "revision": "cb7be80dc797e8d8d1e0",
+    "url": "/static/js/2.ae8b7e66.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.ae8b7e66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9fda492d331374c0be5",
+    "url": "/static/js/main.f81e979c.chunk.js"
+  },
+  {
+    "revision": "d84b6177f08bc6848c92",
+    "url": "/static/js/runtime-main.91effb83.js"
+  }
+]);
